d769fe374206faefb53787692e1044f2830635c9
Henrique Andrade <Henrique.VM.Andrade@gmail.com>
2024-02-29 18:26:15 +0000

    ex2_v2
    factory

---
+++ ES2_Factory/src/com/es2/factorymethod/FactoryProduct.java
@@ -0,0 +1,15 @@
+package com.es2.factorymethod;
+
+public abstract class FactoryProduct extends Object {
+    public FactoryProduct(){}
+
+    public static Product makeProduct(String type) throws UndefinedProductException{
+        if (type.equals("Computer")){
+            return new Computer();
+        } else if (type.equals("Software")) {
+            return new Software();
+        }else {
+            throw new UndefinedProductException();
+        }
+    }
+}
